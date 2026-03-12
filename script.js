33d007050ead9e6a9e2e73b00de140a44215bd98
aarón montoya-moraga <montoyamoraga@gmail.com>
2026-03-12 08:18:31 -0300

    corregir redondear hacia arriba dias actuales, corregir diferencia entre actual e inicio

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ function mesNumeroPalabra(numero) {
 
 // fecha de inicio: 11 abril de 2026 a mediodía
 // fecha final: 11 abril 2030 a mediodía
-const fechaInicio = new Date(2026, 2, 11, 12, 0, 0);
-const fechaFinal = new Date(2030, 2, 11, 12, 0, 0);
+const fechaInicio = new Date(2026, 2, 11, 11, 0, 0);
+const fechaFinal = new Date(2030, 2, 11, 11, 0, 0);
 
 let fechaActual = new Date();
 
@@ -26,6 +26,8 @@ let segundos = document.getElementById("segundos");
 let fin = document.getElementById("fin");
 
 
+// cuando no ha llegado la fecha de inicio
+// mostrar cuánto falta para que llegue la fecha de inicio
 if (fechaActual < fechaInicio) {
 
     const diferenciaHoyInicio = Math.abs(fechaInicio - fechaActual);
@@ -40,13 +42,16 @@ if (fechaActual < fechaInicio) {
     
     inicio.innerHTML = "para que sea " + fechaInicio.getDate() + " de " + mesNumeroPalabra(fechaInicio.getMonth()) + " de " + fechaInicio.getFullYear();
 }
+// si ya pasamos la fecha de inicio
+// mostrar cuántos días han pasado desde la fecha de inicio
 else {
 
-    const diasDesde = Math.floor(Math.abs(fechaActual - fechaInicio) / (1000 * 60 * 60 * 24));
+    // calcular la diferencia y redondear hacia arriba
+    const diasDesde = Math.ceil(Math.abs(fechaActual - fechaInicio) / (1000 * 60 * 60 * 24));
     console.log(diasDesde);
 
     if (diasDesde == 1) {
-        previaQuedan.innerHTML = "han pasado " + diasDesde + " día";
+        previaQuedan.innerHTML = "ha pasado " + diasDesde + " día";
     }
     else {
         previaQuedan.innerHTML = "han pasado " + diasDesde + " días";
@@ -58,7 +63,7 @@ else {
 
 }
 
-const diferenciaFecha = Math.abs(fechaFinal - fechaInicio);
+const diferenciaFecha = Math.abs(fechaFinal - fechaActual);
 
 // 1000 ms * 60 s * 60 m * 24 h
 const diferenciaDias = Math.floor(diferenciaFecha / (1000 * 60 * 60 * 24));
